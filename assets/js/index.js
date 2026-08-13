3c1776b57b99f473d6db58f66dea21848936ec30
Youstina-Edward-70 <youstinaedward1@gmail.com>
2026-08-13 19:03:46 +0300

    feat(index.js): add font toggle functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,8 @@ let htmlElement = document.documentElement;
 let openSettingBtn = document.getElementById("settings-toggle");
 let closeSettingBtn = document.getElementById("close-settings");
 let settingsSidebar = document.getElementById("settings-sidebar");
+let fontOptionBtns = document.querySelectorAll(".font-option");
+let body = document.body;
 
 
 // .
@@ -18,6 +20,24 @@ function showSettingsSidebar() {
 function hideSettingsSidebar() {
     settingsSidebar.classList.add("translate-x-full");
     openSettingBtn.style.right = '0rem';
+}
+function fontToggle(target) {
+    target = target.closest(".font-option");
+    if(target !== null) {
+        let selectedFont = target.dataset.font;
+        body.classList.remove("font-alexandria", "font-tajawal", "font-cairo");
+        body.classList.add(`font-${selectedFont}`);
+
+        for(let i = 0; i < fontOptionBtns.length; i++) {
+            if(fontOptionBtns[i] === target) {
+                fontOptionBtns[i].classList.remove("border-slate-200", "dark:border-slate-700");
+                fontOptionBtns[i].classList.add("active", "border-primary", "bg-slate-50", "dark:bg-slate-800");
+            } else {
+                fontOptionBtns[i].classList.remove("active", "border-primary", "bg-slate-50", "dark:bg-slate-800");
+                fontOptionBtns[i].classList.add("border-slate-200", "dark:border-slate-700");
+            }
+        }
+    }
 }
 
 
@@ -37,3 +57,8 @@ document.addEventListener("click", function(e) {
             hideSettingsSidebar();
     }
 });
+for(let i = 0; i < fontOptionBtns.length; i++) {
+    fontOptionBtns[i].addEventListener("click", function(e) {
+        fontToggle(e.target);
+    })
+}
