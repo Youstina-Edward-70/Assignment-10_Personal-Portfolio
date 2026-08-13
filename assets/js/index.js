f34334224a5930349110f2542bdc7eb87532963c
Youstina-Edward-70 <youstinaedward1@gmail.com>
2026-08-13 16:36:44 +0300

    feat(index.js): add settings sidebar functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,8 @@
 let themeBtn = document.getElementById("theme-toggle-button");
 let htmlElement = document.documentElement;
+let openSettingBtn = document.getElementById("settings-toggle");
+let closeSettingBtn = document.getElementById("close-settings");
+let settingsSidebar = document.getElementById("settings-sidebar");
 
 
 // .
@@ -8,8 +11,29 @@ let htmlElement = document.documentElement;
 function themeToggle() {
     htmlElement.classList.toggle("dark");
 }
+function showSettingsSidebar() {
+    settingsSidebar.classList.remove("translate-x-full");
+    openSettingBtn.style.right = '20rem';
+}
+function hideSettingsSidebar() {
+    settingsSidebar.classList.add("translate-x-full");
+    openSettingBtn.style.right = '0rem';
+}
 
 
 themeBtn.addEventListener("click", function () {
-    themeToggle()
+    themeToggle();
+});
+openSettingBtn.addEventListener("click", function() {
+    showSettingsSidebar();
+});
+closeSettingBtn.addEventListener("click", function() {
+    hideSettingsSidebar();
+});
+document.addEventListener("click", function(e) {
+    if(!settingsSidebar.contains(e.target) 
+        && !openSettingBtn.contains(e.target) 
+        && !settingsSidebar.classList.contains("translate-x-full")) {
+            hideSettingsSidebar();
+    }
 });
