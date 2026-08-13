3471f330d3610619217934f21ebbfbe494fe8a1e
Youstina-Edward-70 <youstinaedward1@gmail.com>
2026-08-13 15:07:53 +0300

    feat: implement theme toggle functionality in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,15 @@
-// ^ Write your JavaScript code here
+let themeBtn = document.getElementById("theme-toggle-button");
+let htmlElement = document.documentElement;
+
+
+// .
+
+
+function themeToggle() {
+    htmlElement.classList.toggle("dark");
+}
+
+
+themeBtn.addEventListener("click", function () {
+    themeToggle()
+});
